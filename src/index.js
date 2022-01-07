509b4f6ba499985f7b5296b31cdafa94c70492f2
tuanhoangle <tuanleprofessional@gmail.com>
2022-01-06 19:51:44 -0500

    remove service worker

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-ServiceWorker.register();
+// ServiceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
